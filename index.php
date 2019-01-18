5f72f56f988e2203f847a25b94574f72c14d80ed
Diego Mazzieri <diego.maz97@gmail.com>
2019-01-18 19:13:23 +0100

    Create Checkout page layout

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
   <link rel="stylesheet" href="css/restaurants.css" />
   <link rel="stylesheet" href="css/menu.css" />
   <link rel="stylesheet" href="css/dish.css" />
+  <link rel="stylesheet" href="css/checkout.css" />
   <!-- scripts -->
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
