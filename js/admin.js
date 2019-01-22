27991a1a17c0f9a79b01ce04db9220b02b2a601a
Diego Mazzieri <diego.maz97@gmail.com>
2019-01-22 18:44:01 +0100

    Create basic js file for the Admin page

---
+++ js/admin.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$(document).on("click", ".add", function() {
+		var emptyInput = $(this).parents("table").find("tr").last().html();
+		var input = $(this).parents("tr").find('input[type="text"]');
+		input.each(function(){
+			$(this).parent("td").html($(this).val());
+		});
+		$(this).parents("tbody").append("<tr>" + emptyInput + "</tr>");
+    });
+	// Edit row on edit button click
+	$(document).on("click", ".edit", function() {
+        $(this).parents("tr").find("td:not(:last-child)").each(function(){
+			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+		});		
+    });
+	// Delete row on delete button click
+	$(document).on("click", ".delete", function(){
+		$(this).parents("tr").remove();
+    });
+});
